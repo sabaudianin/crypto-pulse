12f29b901f0487408cf15ad80456ee71639366c3
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 <sabaudianin@gmail.com>
2026-02-26 14:24:51 +0100

    set new prisma migration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils";
 
 export default function DashboardPage() {
   const [page, setPage] = useState(1);
-  const perPage = 20;
+  const perPage = 50;
   const { data, isLoading, isFetching, isPlaceholderData, isError } = useCoins({
     page: String(page),
     perPage: String(perPage),
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,10 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
 
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
-});
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
